ae236b0360d4d62075f73379f2a61f836c1a0a85
cxmhfut <912750125@qq.com>
2018-06-19 15:16:36 +0800

    update

---
+++ course5/week_03/Test/trigger_word_detection/from_audio_recordings_to_spectrograms_01_2.py
@@ -0,0 +1,13 @@
+from IPython.display import Audio
+from course5.week_03.Test.trigger_word_detection.td_utils import *
+
+Audio("audio_examples/example_train.wav")
+
+x = graph_spectrogram("audio_examples/example_train.wav")
+
+_, data = wavfile.read("audio_examples/example_train.wav")
+print("Time steps in audio recording before spectrogram", data[:,0].shape)
+print("Time steps in input after spectrogram", x.shape)
+
+Tx = 5511 # The number of time steps input to the model from the spectrogram
+n_freq = 101 # Number of frequencies input to the model at each time step of the spectrogram
---
+++ course5/week_03/Test/trigger_word_detection/listening_to_the_data_01_1.py
@@ -0,0 +1,5 @@
+from IPython.display import Audio
+
+Audio("./raw_data/activates/1.wav")
+Audio("./raw_data/negatives/4.wav")
+Audio("./raw_data/backgrounds/1.wav")
